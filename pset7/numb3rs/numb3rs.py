e8fe53c882b5df60a06b8571492121f74fd7c453
obaino <nikos.kyriazis@gmail.com>
2023-09-01 23:16:01 +0300

    work on numb3rs.py

--- pset7/numb3rs/numb3rs.py
+++ pset7/numb3rs/numb3rs.py
@@ -1,2 +1,17 @@
 # https://cs50.harvard.edu/python/2022/psets/7/numb3rs/
 
+import re, sys
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip):
+    # regex = "([0-9]{3})\.([0-9]{3})\.([0-9]{3})\.([0-9]{3})"
+    regex = "\d{1-3}\.\d{1-3}\.\d{1-3}\.\d{1-3}"
+    result = re.match(regex, ip)
+    return result
+
+
+if __name__ == "__main__":
+    main()
