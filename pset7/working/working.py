eae66331caf10dd3f915196146dd9a0e046d1ccc
obaino <nikos.kyriazis@gmail.com>
2023-09-02 22:05:07 +0300

    new file:   pset7/working/working.py

---
+++ pset7/working/working.py
@@ -0,0 +1,17 @@
+# https://cs50.harvard.edu/python/2022/psets/7/working/
+
+import re, sys
+
+def main():
+    print(convert(input("Hours: ")))
+
+
+def convert(s):
+    ...
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
