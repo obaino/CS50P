c075d50dec4183a6464e3634f44c3ac1e99a3cbf
obaino <nikos.kyriazis@gmail.com>
2023-09-02 21:48:48 +0300

    modified:   pset7/watch/watch.py

--- pset7/watch/watch.py
+++ pset7/watch/watch.py
@@ -1,20 +1,17 @@
 # https://cs50.harvard.edu/python/2022/psets/7/watch/
-# input url:
-# <iframe src="https://www.youtube.com/embed/xvFZjo5PgG0"></iframe>
 
 import re, sys
-
 
 def main():
     print(parse(input("HTML: ")))
 
-
 def parse(s):
-    regex = r"src\=\"(http[s]?://www.youtube.com/embed/.+?)\""
-    url_pattern = re.search(regex, s)
-    if url_pattern:
-        return url_pattern.group(1)
-
+    regex = r"src\=\"(http[s]?://(?:www.)?youtube.com/embed/.+?)\""
+    if url_pattern := re.search(regex, s):
+        long_url = url_pattern.group(1)
+        # capture the string after the "embed/"
+        short_url = re.search(r"embed/(.+)$", long_url).group(1)
+        return f"https://youtu.be/{short_url}"
 
 
 if __name__ == "__main__":
