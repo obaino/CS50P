82d542afff9433434c5b98fe4fe748223590545d
macnik <nikos.kyriazis@gmail.com>
2023-09-02 14:33:51 +0300

    modified:   pset7/watch/watch.py

--- pset7/watch/watch.py
+++ pset7/watch/watch.py
@@ -1,4 +1,6 @@
 # https://cs50.harvard.edu/python/2022/psets/7/watch/
+# input url:
+# <iframe src="https://www.youtube.com/embed/xvFZjo5PgG0"></iframe>
 
 import re, sys
 
@@ -8,15 +10,13 @@ def main():
 
 
 def parse(s):
-    regex = r"https?://youtube.com/embed"
-    result = re.search(regex, s)
-    if result:
-        return f"located. starts at {result.start()} ends at {result.end()}"
+    regex = r"(http[s]?://www.youtube.com/embed/\w+)\""
+    url_pattern = re.search(regex, s)
+    if url_pattern:
+        return f"Starts at {url_pattern.start()} ends at {url_pattern.end()}, {url_pattern.group(1)}"
     else:
         return f"Cannot spot it"
 
-
-...
 
 
 if __name__ == "__main__":
