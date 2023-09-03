bd573a220995c852c40e6fac1b45f4dea6d1a373
obaino <nikos.kyriazis@gmail.com>
2023-09-03 09:59:35 +0300

    modified:   pset7/working/working.py

--- pset7/working/working.py
+++ pset7/working/working.py
@@ -4,14 +4,18 @@ import re, sys
 
 def main():
     # print(convert(input("Hours: ")))
+    times = ["10:00 AM", "4:00 PM", "1 PM", "11:00 PM"]
+    for time in times:
+        print(f"input time is: {time}, standard time is: {standard(time)}")
 
 
-def convert(s):
-    ...
+# def convert(s):
+    
 
-
-...
-
+def standard(time):
+    print(len(time.split(":")))
+    if len(time.split(":")) > 1:
+        return time.split(":")
 
 if __name__ == "__main__":
     main()
