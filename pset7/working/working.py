95a8eee75c51427f64e7a406d09e59ec8e64d3b4
obaino <nikos.kyriazis@gmail.com>
2023-09-03 10:43:06 +0300

    finishted standard time funciton

--- pset7/working/working.py
+++ pset7/working/working.py
@@ -4,18 +4,34 @@ import re, sys
 
 def main():
     # print(convert(input("Hours: ")))
-    times = ["10:00 AM", "4:00 PM", "1 PM", "11:00 PM"]
+    times = ["2:00 AM", "8 AM", "9:00 AM", "10:00 AM", "4:00 PM", "1 PM", "10 PM", "11:00 PM"]
     for time in times:
         print(f"input time is: {time}, standard time is: {standard(time)}")
+        print("----" * 12)
 
 
 # def convert(s):
     
 
 def standard(time):
-    print(len(time.split(":")))
+    output_time = ""
     if len(time.split(":")) > 1:
-        return time.split(":")
+        hours = int(time.split(":")[0])
+        minutes = time.split(":")[1]
+        if minutes[-2:] == "PM":
+            hours += 12
+            output_time = f"{hours}:{minutes[:-3]}"
+        else:
+            output_time = f"{hours:02}:{minutes[:-3]}"
+    else:
+        hours = int(time.split(" ")[0])
+        if time[-2:] == "PM":
+            hours += 12
+            output_time = f"{hours}:00"
+        else:
+            output_time = f"{hours:02}:00"
+    return output_time
+
 
 if __name__ == "__main__":
     main()
