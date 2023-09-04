3faba698102492ce6ef2ce89da7af0b6b444b8a9
macnik <nikos.kyriazis@gmail.com>
2023-09-04 19:39:03 +0300

    modified:   pset7/working/test_working.py 	modified:   pset7/working/working.py

--- pset7/working/test_working.py
+++ pset7/working/test_working.py
@@ -1,13 +1,14 @@
 
 import pytest
-from working import convert, standard
+from working import convert
 
 def test_time_input():
     with pytest.raises(ValueError):
-        convert("9:00 PM - 8:00 AM")
-        convert("8 AM to 12:61 PM")
+        convert("17 AM")
+        convert("12:61 PM")
 
 def test_time_output():
-    assert convert("10:30 PM to 8:50 AM") == "22:30 to 08:50"
-    assert convert("10 PM to 8 AM") == "22:00 to 08:00"
+    assert convert("10:30 PM") == "22:30"
+    assert convert("10 PM") == "22:00"
+    assert convert("12:19 AM") == "00:19"
 
--- pset7/working/working.py
+++ pset7/working/working.py
@@ -13,35 +13,25 @@ def convert(s):
     regex = r"^(0?[1-9]|1[0-2])(?::([0-5][0-9]))? (AM|PM)$"
     try:
         if time := re.search(regex, s):
-            if time.group(2) == None: # time format is: hh AM/PM
+            if time.group(2) == None:
                 new_time = f"{time.group(1)}:00 {time.group(3)}"
-            else: # time format is: hh:mm AM/PM
+            else:
                 new_time = f"{time.group(1)}:{time.group(2)} {time.group(3)}"
-            return standard(new_time)
+            hours = int(new_time.split(":")[0])
+            minutes = new_time.split(":")[1]
+            if minutes[-2:] == "PM":
+                hours += 12
+                output_time = f"{hours}:{minutes[:-3]}"
+            elif hours == 12:
+                output_time = f"00:{minutes[:-3]}"
+            else:
+                output_time = f"{hours:02}:{minutes[:-3]}"
+
+            return output_time
         else:
             raise ValueError
     except ValueError:
         exit()
-
-def standard(time):
-    output_time = ""
-    if len(time.split(":")) > 1:
-        hours = int(time.split(":")[0])
-        minutes = time.split(":")[1]
-        if minutes[-2:] == "PM":
-            hours += 12
-            output_time = f"{hours}:{minutes[:-3]}"
-        else:
-            output_time = f"{hours:02}:{minutes[:-3]}"
-    else:
-        hours = int(time.split(" ")[0])
-        if time[-2:] == "PM":
-            hours += 12
-            output_time = f"{hours}:00"
-        else:
-            output_time = f"{hours:02}:00"
-    return output_time
-
 
 if __name__ == "__main__":
     main()
