ef6304036502be42c1af1e186cdb46da0b7734a6
macnik <nikos.kyriazis@gmail.com>
2023-09-05 10:19:42 +0300

    new file:   pset7/working/working.1.py 	new file:   pset7/working/working.2.py 	modified:   pset7/working/working.py

---
+++ pset7/working/working.1.py
@@ -0,0 +1,47 @@
+# https://cs50.harvard.edu/python/2022/psets/7/working/
+
+import re, sys
+
+def main():
+    try:
+        hour1, hour2 = input(("Hours: ")).split(" to ")
+        print(f"{convert(hour1)} to {convert(hour2)}")
+    except ValueError:
+        exit()
+
+def convert(s):
+    regex = r"^(0?[1-9]|1[0-2])(?::([0-5][0-9]))? (AM|PM)$"
+    try:
+        if time := re.search(regex, s):
+            if time.group(2) == None: # time format is: hh AM/PM
+                new_time = f"{time.group(1)}:00 {time.group(3)}"
+            else: # time format is: hh:mm AM/PM
+                new_time = f"{time.group(1)}:{time.group(2)} {time.group(3)}"
+            return standard(new_time)
+        else:
+            raise ValueError
+    except ValueError:
+        exit()
+
+def standard(time):
+    output_time = ""
+    if len(time.split(":")) > 1:
+        hours = int(time.split(":")[0])
+        minutes = time.split(":")[1]
+        if minutes[-2:] == "PM":
+            hours += 12
+            output_time = f"{hours}:{minutes[:-3]}"
+        else:
+            output_time = f"{hours:02}:{minutes[:-3]}"
+    else:
+        hours = int(time.split(" ")[0])
+        if time[-2:] == "PM":
+            hours += 12
+            output_time = f"{hours}:00"
+        else:
+            output_time = f"{hours:02}:00"
+    return output_time
+
+
+if __name__ == "__main__":
+    main()
--- pset7/working/working.py
+++ pset7/working/working.py
@@ -3,35 +3,50 @@
 import re, sys
 
 def main():
-    try:
-        hour1, hour2 = input(("Hours: ")).split(" to ")
-        print(f"{convert(hour1)} to {convert(hour2)}")
-    except ValueError:
-        exit()
+    print(convert(input("Hours: ")))
 
 def convert(s):
-    regex = r"^(0?[1-9]|1[0-2])(?::([0-5][0-9]))? (AM|PM)$"
-    try:
-        if time := re.search(regex, s):
-            if time.group(2) == None:
-                new_time = f"{time.group(1)}:00 {time.group(3)}"
-            else:
-                new_time = f"{time.group(1)}:{time.group(2)} {time.group(3)}"
-            hours = int(new_time.split(":")[0])
-            minutes = new_time.split(":")[1]
+    regex = r"^(?P<hr_1>0?[1-9]|1[0-2])(?::(?P<min_1>[0-5][0-9]))? (?P<ap_1>AM|PM) to (?P<hr_2>0?[1-9]|1[0-2])(?::(?P<min_2>[0-5][0-9]))? (?P<ap_2>AM|PM)$"
+    if time := re.search(regex, s, flags=re.IGNORECASE):
+        hr_1, min_1, ap_1, hr_2, min_2, ap_2 = time.groups()
+        print(hr_1, min_1, ap_1, hr_2, min_2, ap_2)
+
+        # if time.group(2) == None:
+        #     new_time = f"{time.group(1)}:00 {time.group(3)}"
+        # else:
+        #     new_time = f"{time.group(1)}:{time.group(2)} {time.group(3)}"
+        # hours = int(new_time.split(":")[0])
+        # minutes = new_time.split(":")[1]
+        # if minutes[-2:] == "PM":
+        #     hours += 12
+        #     output_time = f"{hours}:{minutes[:-3]}"
+        # elif hours == 12:
+        #     output_time = f"00:{minutes[:-3]}"
+        # else:
+        #     output_time = f"{hours:02}:{minutes[:-3]}"
+
+        # return output_time
+    else:
+        raise ValueError
+    
+    def standard(time):
+        output_time = ""
+        if len(time.split(":")) > 1:
+            hours = int(time.split(":")[0])
+            minutes = time.split(":")[1]
             if minutes[-2:] == "PM":
                 hours += 12
                 output_time = f"{hours}:{minutes[:-3]}"
-            elif hours == 12:
-                output_time = f"00:{minutes[:-3]}"
             else:
                 output_time = f"{hours:02}:{minutes[:-3]}"
-
-            return output_time
         else:
-            raise ValueError
-    except ValueError:
-        exit()
+            hours = int(time.split(" ")[0])
+            if time[-2:] == "PM":
+                hours += 12
+                output_time = f"{hours}:00"
+            else:
+                output_time = f"{hours:02}:00"
+        return output_time
 
 if __name__ == "__main__":
     main()
