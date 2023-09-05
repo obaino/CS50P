47592ef385b045f9adf7588049cd8d7cc8e250b0
obaino <nikos.kyriazis@gmail.com>
2023-09-05 23:19:14 +0300

    formatted

--- pset5/plates.py
+++ pset5/plates.py
@@ -1,12 +1,14 @@
 # rewritten for the test_plates.py
 # https://cs50.harvard.edu/python/2022/psets/5/test_plates/
 
+
 def main():
     plate = input("Plate: ")
     if is_valid(plate):
         print("Valid")
     else:
         print("Invalid")
+
 
 def is_valid(s):
     return (
@@ -16,11 +18,14 @@ def is_valid(s):
         and punctuation_check(s)
     )
 
+
 def two_letters(text):
     return text[:2].isalpha()
 
+
 def text_length(text):
     return 2 <= len(text) <= 6
+
 
 def check_digits_after_digit(text):
     if text.isalpha():
@@ -41,13 +46,15 @@ def check_digits_after_digit(text):
             else:
                 return False
 
-def punctuation_check(text):
-        return not any(c in "'.,;:?! " for c in text)
-        # for c in "'.,;:?! ":
-        #     if c in text:
-        #         return False
-        # else:
-        #     return True
 
-if __name__=="__main__":
+def punctuation_check(text):
+    return not any (c in "'.,;:?! " for c in text)
+    # for c in "'.,;:?! ":
+    #     if c in text:
+    #         return False
+    # else:
+    #     return True
+
+
+if __name__ == "__main__":
     main()
