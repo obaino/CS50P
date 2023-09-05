44dc5d6ea3b36a0c7bfa203a013438d0b2e28c29
macnik <nikos.kyriazis@gmail.com>
2023-09-05 15:45:58 +0300

    updated up.py

--- pset7/um/um.py
+++ pset7/um/um.py
@@ -1,17 +1,33 @@
 # https://cs50.harvard.edu/python/2022/psets/7/um/
 
-import re, sys
-
+import re
 
 def main():
-    print(count(input("Text: ")))
+    # print(count(input("Text: ")))
+
+    # test phrases
+    test_phrases = [
+        "hello, um, everybody",
+        "trumpet",
+        "the, um, tin drum",
+        "little drummer boy",
+        "UM, may I have your, um, attention plesase",
+        "um?",
+        "Um, thanks for the album.",
+        "Um, thanks, um...",
+    ]
+    # test the phrases
+    for test_phrase in test_phrases:
+        print(test_phrase)
+        print(count(test_phrase))
+        print("---" * 14)
+
 
 
 def count(s):
-    ...
-
-
-...
+    regex = r"\bum\b"
+    counter = re.findall(regex, s, flags=re.IGNORECASE)
+    return len(counter)
 
 
 if __name__ == "__main__":
