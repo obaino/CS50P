30eb921ac7950def4c24ce5bf4fd031a71e09ccd
macnik <nikos.kyriazis@gmail.com>
2023-09-05 17:36:32 +0300

    solved

--- pset7/response.py
+++ pset7/response.py
@@ -1,1 +1,17 @@
 # https://cs50.harvard.edu/python/2022/psets/7/response/
+
+import validators
+
+def main():
+    email = input("What's your email address? ")
+    print(validate(email))
+
+def validate(text):
+    if validators.email(text):
+        return f"Valid"
+    else:
+        return f"Invalid"
+
+
+if __name__ == "__main__":
+    main()
