e10f00738e4e901be2d53b5056633f4b34cf6703
macnik <nikos.kyriazis@gmail.com>
2023-09-05 14:38:55 +0300

    passed

--- pset7/working/working.py
+++ pset7/working/working.py
@@ -5,48 +5,45 @@ import re, sys
 def main():
     print(convert(input("Hours: ")))
 
+    # testing_hours = [
+    #     "9 AM to 5 PM",
+    #     "9:00 AM to 5:00 PM",
+    #     "10 PM to 8 AM",
+    #     "10:30 PM to 8:50 AM",
+    #     "12:13 AM to 4 PM",
+    #     "3 AM to 4:23 PM",
+    #     "12:05 PM to 8:30 PM",
+    #     "12:20 AM to 3:30 PM",
+    #     "12:20 AM - 3:30 PM"
+    #     ]
+
+    # for testing_hour in testing_hours:
+    #     print(testing_hour)
+    #     print(convert(testing_hour))
+    #     print("---" * 14)
+
 def convert(s):
-    regex = r"^(?P<hr_1>0?[1-9]|1[0-2])(?::(?P<min_1>[0-5][0-9]))? (?P<ap_1>AM|PM) to (?P<hr_2>0?[1-9]|1[0-2])(?::(?P<min_2>[0-5][0-9]))? (?P<ap_2>AM|PM)$"
+    regex = r"^(?P<hours1>0?[1-9]|1[0-2])(?::(?P<minutes1>[0-5][0-9]))? (?P<ap_1>AM|PM) to (?P<hours2>0?[1-9]|1[0-2])(?::(?P<minutes2>[0-5][0-9]))? (?P<ap_2>AM|PM)$"
     if time := re.search(regex, s, flags=re.IGNORECASE):
-        hr_1, min_1, ap_1, hr_2, min_2, ap_2 = time.groups()
-        print(hr_1, min_1, ap_1, hr_2, min_2, ap_2)
-
-        # if time.group(2) == None:
-        #     new_time = f"{time.group(1)}:00 {time.group(3)}"
-        # else:
-        #     new_time = f"{time.group(1)}:{time.group(2)} {time.group(3)}"
-        # hours = int(new_time.split(":")[0])
-        # minutes = new_time.split(":")[1]
-        # if minutes[-2:] == "PM":
-        #     hours += 12
-        #     output_time = f"{hours}:{minutes[:-3]}"
-        # elif hours == 12:
-        #     output_time = f"00:{minutes[:-3]}"
-        # else:
-        #     output_time = f"{hours:02}:{minutes[:-3]}"
-
-        # return output_time
+        hours1, minutes1, ap_1, hours2, minutes2, ap_2 = time.groups()
+        return f"{standard_time(hours1, minutes1, ap_1)} to {standard_time(hours2, minutes2, ap_2)}"
     else:
         raise ValueError
-    
-    def standard(time):
-        output_time = ""
-        if len(time.split(":")) > 1:
-            hours = int(time.split(":")[0])
-            minutes = time.split(":")[1]
-            if minutes[-2:] == "PM":
-                hours += 12
-                output_time = f"{hours}:{minutes[:-3]}"
-            else:
-                output_time = f"{hours:02}:{minutes[:-3]}"
+
+def standard_time(hours, minutes, ap):
+    if minutes == None:
+        minutes = "00"
+    if ap == "PM":
+        if hours == "12":
+            hours == "12"
         else:
-            hours = int(time.split(" ")[0])
-            if time[-2:] == "PM":
-                hours += 12
-                output_time = f"{hours}:00"
-            else:
-                output_time = f"{hours:02}:00"
-        return output_time
+            hours = int(hours) + 12
+    else:
+        if hours == "12":
+            hours = "00"
+        else:
+            hours = int(hours)
+    return f"{hours:02}:{minutes}"
 
 if __name__ == "__main__":
     main()
