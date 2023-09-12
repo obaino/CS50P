30fad505b0f6dda669d7a019b109d536547957a5
macnik <nikos.kyriazis@gmail.com>
2023-09-12 15:22:11 +0300

    modified:   pset8/seasons/seasons.py 	modified:   pset8/seasons/test_seasons.py

--- pset8/seasons/seasons.py
+++ pset8/seasons/seasons.py
@@ -5,24 +5,23 @@ from datetime import date
 
 
 def main():
+    today = date.today()
+    birthday = input("Date of birth: ")
+    print(get_minutes(birthday, today))
+
+
+def get_minutes(birthday, now):
     p = inflect.engine()
-    output = p.number_to_words(get_dob(), andword="")
-    print(f"{output} minutes")
-
-
-def get_dob():
     try:
-        today = date.today()
-        # create a list of integers from the "-" splitted date
-        # and instanciate a date object from user's birthday
-        bd = list(map(int, input("Date of birth: ").split("-")))
-        dob = date(bd[0], bd[1], bd[2])
-        # get timedifference between dates and return the tolal
-        # in seconds, then minutes
-        timedelta = today - dob
-        return round(timedelta.total_seconds() / 60)
+        bd = list(map(int, birthday.split("-")))
+        date_of_birth = date(bd[0], bd[1], bd[2])
+        timedelta = now - date_of_birth
+        output = p.number_to_words(
+            round(timedelta.total_seconds() / 60), andword=""
+        ).capitalize()
+        return f"{output} minutes"
     except ValueError:
-        exit("Invalid Date")
+        sys.exit("Invalid Date")
 
 
 if __name__ == "__main__":
--- pset8/seasons/test_seasons.py
+++ pset8/seasons/test_seasons.py
@@ -1,5 +1,22 @@
 import pytest
-from seasons import get_dob, find_minutes
+from seasons import get_minutes
+from datetime import date, timedelta
 
-def test_birthday():
-    assert get_dob("1972-02-27") == "1972-02-27"
+
+def test_get_minutes():
+    assert get_minutes(str(date.today()-timedelta(days=365)), date.today()) == "Five hundred twenty-five thousand, six hundred minutes"
+    assert get_minutes(str(date.today()-timedelta(days=730)), date.today()) == "One million, fifty-one thousand, two hundred minutes"
+
+def test_System_Exit():
+    # with pytest.raises(SystemExit):
+    #     get_minutes("spam", date.today())
+    with pytest.raises(SystemExit):
+        get_minutes("2024-09-10", date.today())
+    with pytest.raises(SystemExit):
+        get_minutes("2023-13-10", date.today())
+    with pytest.raises(SystemExit):
+        get_minutes("2023-12-32", date.today())
+    with pytest.raises(SystemExit):
+        get_minutes("nikos-nk-kn", date.today())
+    with pytest.raises(SystemExit):
+        get_minutes(" 2022-13-12", date.today())
