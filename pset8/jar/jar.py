595b2817f507f7e27b970ba9b19b28f06d02412c
obaino <nikos.kyriazis@gmail.com>
2023-09-12 23:26:18 +0300

    modified:   pset8/jar/jar.py

--- pset8/jar/jar.py
+++ pset8/jar/jar.py
@@ -1,44 +1,50 @@
 # https://cs50.harvard.edu/python/2022/psets/8/jar/
 
+
 class Jar:
     def __init__(self, capacity=12, size=0):
-        '''Initialize the Jar object'''
-        self.capacity = capacity
-        self.size = size
+        """Initialize the Jar object"""
+        self._capacity = capacity
+        self._size = size
 
     def __str__(self):
-        '''return a string containing n cookies in the jar'''
-        return f"{self.size * '🍪'}, {self.capacity}"
+        """return a string containing n cookies in the jar"""
+        return f"{self.size * '🍪'}"
 
     def deposit(self, n):
-        '''Deposit cookies in the jar'''
-        self.size = self.size + n
-        return self.size
+        """Deposit cookies in the jar"""
+        self._size = self._size + n
+        return self._size
 
     def withdraw(self, n):
-        '''Withdraw cookies from the jar'''
-        self.size = self.size - n
-        return self.size
+        """Withdraw cookies from the jar"""
+        self._size = self._size - n
+        return self._size
 
     # Getter for capacity
     @property
     def capacity(self):
+        print(f"{self._capacity} was accessed")
         return self._capacity
 
     # Setter for capacity
     @capacity.setter
     def capacity(self, capacity):
+        print(f"setter for capacity: {self._capacity} value is now: {capacity}")
         self._capacity = capacity
 
     # Getter for size
     @property
     def size(self):
+        print(f"getter size {self._size} was accessed")
         return self._size
 
-    # Setter for capacity
+    # Setter for size
     @size.setter
     def size(self, size):
+        print(f"setter for size: {self._size} value is now: {size}")
         self._size = size
+
 
 def main():
     cookies = Jar(12)
@@ -47,7 +53,9 @@ def main():
     cookies.withdraw(4)
     cookies.withdraw(3)
     cookies.deposit(7)
+    cookies.withdraw(6)
     print(cookies)
+
 
 if __name__ == "__main__":
     main()
