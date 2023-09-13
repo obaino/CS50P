89ea5d50ecee8f76dc5311b37f75357a1619bb0d
macnik <nikos.kyriazis@gmail.com>
2023-09-13 18:53:18 +0300

    testing shirtificate.py

--- pset8/shirtificate/shirtificate.py
+++ pset8/shirtificate/shirtificate.py
@@ -1,4 +1,14 @@
 # https://cs50.harvard.edu/python/2022/psets/8/shirtificate/
 
-import fpdf2
+from fpdf import FPDF
+from PIL import Image
 
+img = Image.open("shirtificate.png")
+shirt_pdf = FPDF("P", "mm", "A4")
+shirt_pdf.add_page()
+shirt_pdf.set_font("helvetica", "", 40)
+shirt_pdf.cell(190, 15, "CS50 Shirtificate", align="C", new_x="LEFT", new_y="NEXT")
+shirt_pdf.image(img)
+
+
+shirt_pdf.output("shirt_cs50.pdf")
