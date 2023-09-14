f6510cf3bdd3699d74c793f54618843138573699
obaino <nikos.kyriazis@gmail.com>
2023-09-14 21:34:47 +0300

    deleted:    pset8/shirtificate/shirt_cs50.pdf 	modified:   pset8/shirtificate/shirtificate.py

--- pset8/shirtificate/shirtificate.py
+++ pset8/shirtificate/shirtificate.py
@@ -3,18 +3,20 @@
 from fpdf import FPDF, Align
 from PIL import Image, ImageFont, ImageDraw
 
-text = "Nikos Kyriazis took CS50"
+user_name = input("Name: ")
+text = f"{user_name} took CS50"
 
 img = Image.open("shirtificate.png")
-myFont = ImageFont.truetype("Helvetica", size=24)
+myFont = ImageFont.truetype("Arial", size=24)
 new_img = ImageDraw.Draw(img)
-new_img = img.resize((round(img.size[0] * 0.91) , round(img.size[1] * 0.91)))
+new_img = img.resize((round(img.size[0] * 0.91), round(img.size[1] * 0.91)))
 width, height = new_img.size
 length = myFont.getlength(text)
-print(width, height, length)
-ImageDraw.Draw(new_img).text(((width - length) / 2, (height - 25) / 3), text, fill =(255, 255, 255),font=myFont)
+ImageDraw.Draw(new_img).text(
+    ((width - length) / 2, (height - 25) / 3), text, fill=(255, 255, 255), font=myFont
+)
 
-#pdf
+# pdf
 shirt_pdf = FPDF("P", "mm", "A4")
 shirt_pdf.add_page()
 shirt_pdf.set_font("helvetica", "", 48)
@@ -22,4 +24,4 @@ shirt_pdf.cell(0, 50, "CS50 Shirtificate", align="C")
 shirt_pdf.image(new_img, x=Align.C, y=70)
 
 
-shirt_pdf.output("shirt_cs50.pdf")
+shirt_pdf.output("shirtificate.pdf")
