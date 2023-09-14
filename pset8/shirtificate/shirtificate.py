5d20350bc39ff3df0844b32c239d0807c473a093
macnik <nikos.kyriazis@gmail.com>
2023-09-14 19:47:14 +0300

    new file:   pset8/shirtificate/shirt_cs50.pdf 	modified:   pset8/shirtificate/shirtificate.py

--- pset8/shirtificate/shirtificate.py
+++ pset8/shirtificate/shirtificate.py
@@ -1,14 +1,25 @@
 # https://cs50.harvard.edu/python/2022/psets/8/shirtificate/
 
-from fpdf import FPDF
-from PIL import Image
+from fpdf import FPDF, Align
+from PIL import Image, ImageFont, ImageDraw
+
+text = "Nikos Kyriazis took CS50"
 
 img = Image.open("shirtificate.png")
+myFont = ImageFont.truetype("Helvetica", size=24)
+new_img = ImageDraw.Draw(img)
+new_img = img.resize((round(img.size[0] * 0.91) , round(img.size[1] * 0.91)))
+width, height = new_img.size
+length = myFont.getlength(text)
+print(width, height, length)
+ImageDraw.Draw(new_img).text(((width - length) / 2, (height - 25) / 3), text, fill =(255, 255, 255),font=myFont)
+
+#pdf
 shirt_pdf = FPDF("P", "mm", "A4")
 shirt_pdf.add_page()
 shirt_pdf.set_font("helvetica", "", 48)
-shirt_pdf.cell(0, 15, "CS50 Shirtificate", align="C", new_x="LEFT", new_y="NEXT")
-shirt_pdf.image(img, 0)
+shirt_pdf.cell(0, 50, "CS50 Shirtificate", align="C")
+shirt_pdf.image(new_img, x=Align.C, y=70)
 
 
 shirt_pdf.output("shirt_cs50.pdf")
